1a7c63215ddd917da45edfb1cc9e153e396afd9e
nathanael <nathanaelbrenno@gmail.com>
2024-05-02 09:15:08 -0300

    update 3  calculator

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 
-let display = document.getElementById('display');
-let resultDisplay = document.getElementById('resultDisplay');
+// let display = document.getElementById('display');
+// let resultDisplay = document.getElementById('resultDisplay');
 let temporaryNumber = '';
-let SecondValue = '';
 let firstValue = 0;
 let ativo = false;
 
@@ -13,9 +12,8 @@ function clean() {
 }
 
 function cleanLastElement() {
-  // temporaryNumber = [];
   display.value = display.value.slice(0, -1);
-  // temporaryNumber = display.value;
+  resultDisplay = resdisplay.value.slice(0, -1)
 }
 
 function operationsMath(expressao = null) { /* é preciso adicionar um parametro nulo, para que a funçao funcione mesmo se for chamada sem um parâmetro */
@@ -63,13 +61,11 @@ function hasParantese() {
   if (ativo == false) {
     display.value += "(";
     ativo = true;
-    // display.value += temporaryNumber;
     console.log(ativo);
   }
   else {
     display.value += ")";
     ativo = false;
-    // display.value += temporaryNumber;
     console.log(ativo);
   }
 }
@@ -78,28 +74,17 @@ function btnClick(num) {
   const number = Number(num);
   let guarda = '';
   if (number >= 0) {
-    // temporaryNumber += num;
-    console.log(temporaryNumber, 'leo');
     display.value += num;
     guarda = display.value.match((/(\d+\.\d+|\d+|[%\+\-\x\÷\(\),])/g));
     console.log(isImpar(guarda.length, display.value));
     return;
   }
 
-  // if ((num === '+' || num === '-' || num === '÷' || num === 'x' || num === '%' || num === ',') || num === '()') {
-  //   temporaryNumber += num;
-  //   display.value = temporaryNumber;
-  //   guarda = display.value.match((/(\d+\.\d+|\d+|[%\+\-\x\÷\(\),])/g));
-  //   console.log(isImpar(guarda.length, display.value));
-  //   return;
-  // }
-
   if (num === '=') {
     temporaryNumber = operationsMath(temporaryNumber);
-    // console.log("op", temporaryNumber);
-    resultDisplay.value = temporaryNumber;
-    guarda = resultDisplay.value.match((/(\d+\.\d+|\d+|[%\+\-\x\÷\(\),])/g));
-    // console.log(isImpar(guarda.length, display.value))
+    // guarda = resultDisplay.value.match((/(\d+\.\d+|\d+|[%\+\-\x\÷\(\),])/g));
+    resultDisplay.value = '';
+    display.value = temporaryNumber;
   }
 }
 
@@ -113,13 +98,7 @@ function isImpar(num) {
 
 function verifyOperator(num) {
   return num === '+' || num === '-' || num === '÷' || num === 'x' || num === '%' || num === ',' || num === '()'
-  // temporaryNumber += num;
-  // resultDisplay.value = temporaryNumber;
-  // guarda = display.value.match((/(\d+\.\d+|\d+|[%\+\-\x\÷\(\),])/g));
-  // console.log(isImpar(guarda.length, display.value));
-  // return;
 }
-
 
 function operators(num) {
   let armazena = display.value;
@@ -130,7 +109,6 @@ function operators(num) {
   }
   display.value += num
   console.log(lastCaractere, 'aqui');
-
 }
 
 // function calcular() {
@@ -160,11 +138,11 @@ function operators(num) {
 //       continue;
 //     }
 
-//     if (element === 'x'  element === '/') {
+//     if (element === 'x' || element === '/') {
 //       currentoperator = element;
 //     }
 
-//     if (element === '+'  element === '-') {
+//     if (element === '+' || element === '-') {
 //       stack.push(element);
 //     }
 //   }
@@ -207,97 +185,3 @@ function operators(num) {
 
 //   // }
 // }
-
-
-// function hasParantese(num) {
-//   let elements = temporaryNumber.split(' ');
-//   console.log(elements);
-//   parenteses = false;
-//   for (i = 0; i < num.length; i++) {
-//     if (num === "()") {
-//       temporaryNumber += "(";
-//       display.value = temporaryNumber;
-//       console.log('entrou ()');
-//     }
-//     for (i = 0; i < num.length; i++) 
-//     if (temporaryNumber === "(") {
-//       temporaryNumber += ")"
-//       display.value = temporaryNumber;
-//       console.log(display.value);
-//       console.log('possui (');
-//     }
-//     console.log(display.value);
-//     return;
-//   }
-// }
-
-
-// switch (num) {
-//   case !isNaN(num):
-//     temporaryNumber += num;
-//     temporaryNumber += num
-//     document.getElementById('display').value = temporaryNumber;
-//     break;
-//   case (num === '+' || num === '-' || num === '÷' || num === 'x' || num === '()' || num === '%'): /* Se o num for um operador de soma ou de igual */
-//     temporaryNumber += ' ' + num + ' '; /**/
-//     document.getElementById('display').setAttribute("value", temporaryNumber);
-//     break;
-//   case (num === '='):  /* Se não houver um operador previamente definido */
-//     temporaryNumber += operationsMath(temporaryNumber) /* Insere a função onde tem os cálculos matemáticos dentro de opertador */
-//     document.getElementById('firstValue-display').setAttribute("value", temporaryNumber); /*Atualiza o valor do display */
-//     break;
-// }
-
-
-
-
-
-
-// function btnClick(num) {
-//   switch (true) {
-//     case !isNaN(num): // Se o num for um número
-//       temporaryNumber += num;
-//       temporaryNumber += num;
-//       document.getElementById('display').setAttribute("value", temporaryNumber);
-//       break;
-//     case (num === '+' || num === '-' || num === '*' || num === '/'): // Se o num for um operador
-//       temporaryNumber += ' ' + num + ' ';
-//       document.getElementById('display').setAttribute("value", temporaryNumber);
-//       break;
-//     case (num === '='): // Se o num for igual
-//       temporaryNumber = evaluateExpression(temporaryNumber);
-//       document.getElementById('display').setAttribute("value", temporaryNumber);
-//       temporaryNumber = '';
-//       break;
-//   }
-// }
-
-// function evaluateExpression(temporaryNumber) {
-//   let tokens = temporaryNumber.split(' ');
-//   let firstValue = parseFloat(tokens[0]);
-
-//   for (let i = 1; i < tokens.length; i += 2) {
-//     let temporaryNumber = tokens[i];
-//     let nextNumber = parseFloat(tokens[i + 1]);
-
-//     switch (temporaryNumber) {
-//       case '+':
-//         firstValue += nextNumber;
-//         break;
-//       case '-':
-//         firstValue -= nextNumber;
-//         break;
-//       case '*':
-//         firstValue *= nextNumber;
-//         break;
-//       case '/':
-//         firstValue /= nextNumber;
-//         break;
-//     }
-//   }
-
-//   return firstValue;
-// }
-
-
-
